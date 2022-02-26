e203b2c9381025089a3cb6a313db81f6104a49ae
Gilberto <gilbertocruz2007@micorreo.upp.edu.mx>
2022-02-26 17:55:12 -0600

    Constructor, getter and setter y to String de la clase Provedor creados

--- Provedor.java
+++ Provedor.java
@@ -10,4 +10,53 @@ package ejerciciorelaciones;
  */
 public class Provedor {
     
+    private String razonSocial;
+    private String direccionFiscal;
+    private String rfc;
+    private String cadenas;
+
+    public Provedor(String razonSocial, String direccionFiscal, String rfc, String cadenas) {
+        this.razonSocial = razonSocial;
+        this.direccionFiscal = direccionFiscal;
+        this.rfc = rfc;
+        this.cadenas = cadenas;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDireccionFiscal() {
+        return direccionFiscal;
+    }
+
+    public void setDireccionFiscal(String direccionFiscal) {
+        this.direccionFiscal = direccionFiscal;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getCadenas() {
+        return cadenas;
+    }
+
+    public void setCadenas(String cadenas) {
+        this.cadenas = cadenas;
+    }
+
+    @Override
+    public String toString() {
+        return "Provedor{ " + "Razon social: " + razonSocial + ", Direccion fiscal: " + direccionFiscal + ", RFC: " + rfc + ", Cadenasa las que Distribuye: " + cadenas + " }";
+    }        
+    
 }
