75a98b96ce1a50c08b43a701470fed979cae2ee9
Gilberto <gilbertocruz2007@micorreo.upp.edu.mx>
2022-02-26 18:06:09 -0600

    Constructor, getter and setter y to String de la clase Sucursal creados

--- Sucursal.java
+++ Sucursal.java
@@ -10,4 +10,63 @@ package ejerciciorelaciones;
  */
 public class Sucursal {
     
+    private String nombre;
+    private String direccion;
+    private int numeroEmpleados;
+    private CuentaBancaria cuenta;
+    private Provedor provedores;
+
+    public Sucursal(String nombre, String direccion, int numeroEmpleados, CuentaBancaria cuenta, Provedor provedores, String clave, float monto) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.numeroEmpleados = numeroEmpleados;
+        this.cuenta = new CuentaBancaria(clave, monto);
+        this.provedores = provedores;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNumeroEmpleados() {
+        return numeroEmpleados;
+    }
+
+    public void setNumeroEmpleados(int numeroEmpleados) {
+        this.numeroEmpleados = numeroEmpleados;
+    }
+
+    public CuentaBancaria getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(CuentaBancaria cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public Provedor getProvedores() {
+        return provedores;
+    }
+
+    public void setProvedores(Provedor provedores) {
+        this.provedores = provedores;
+    }
+
+    @Override
+    public String toString() {
+        return "Sucursal{ " + "Nombre: " + nombre + ", Direccion: " + direccion + ", Numero de empleados: " + numeroEmpleados + ", Cuenta: " + cuenta + ", Provedores:" + provedores + " }";
+    }        
+    
 }
