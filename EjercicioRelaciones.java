12921735a57c96f8a7ee31d7e78434c685358a4d
Gilberto <gilbertocruz2007@micorreo.upp.edu.mx>
2022-02-27 12:28:54 -0600

    Clase Rpincipal (main) segundo parcial (terminar)

--- EjercicioRelaciones.java
+++ EjercicioRelaciones.java
@@ -33,7 +33,7 @@ public class EjercicioRelaciones {
             System.out.println("+--------------------------------------------------------------------------------+");
             System.out.println("    MENU");
             System.out.println("|--------------------------------------------------------------------------------|");
-            System.out.println("        (1) Ver Sucursales");
+            System.out.println("        (1) Ver Informacion De Las Sucursales");
             System.out.println("        (2) Editar Informacion De Las Sucursales");
             System.out.println("        (3) SALIR");
             System.out.println("|--------------------------------------------------------------------------------|");
@@ -47,7 +47,7 @@ public class EjercicioRelaciones {
                     do{
                         
                         System.out.println("+--------------------------------------------------------------------------------+");
-                        System.out.println("    MENU");
+                        System.out.println("    SUBMENU ----> Ver Informacion De Las Sucursales");
                         System.out.println("|--------------------------------------------------------------------------------|");
                         System.out.println("        (1) "+Pachuca_Norte.getNombre());
                         System.out.println("        (2) "+Pachuca_Centro.getNombre());
@@ -61,19 +61,57 @@ public class EjercicioRelaciones {
                         
                         switch (caso) {
                             case 1:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("        Nombre: " + Pachuca_Norte.getNombre());
+                                System.out.println("|--------------------------------------------------------------------------------|");
+                                System.out.println("        Direccion: " + Pachuca_Norte.getDireccion());
+                                System.out.println("        Numero De empleados: " + Pachuca_Norte.getNumeroEmpleados());
+                                System.out.println("        Cuenta: " + Pachuca_Norte.getCuenta());
+                                System.out.println("        Provedores: " + Provedor1.getRazonSocial());
+                                System.out.println("                    " + Provedor1.getDireccionFiscal());
+                                System.out.println("                    " + Provedor1.getRfc());
+                                System.out.println("                    " + Provedor1.getCadenas());
+                                System.out.println("+--------------------------------------------------------------------------------+");
                                 break;                                
                             } 
                             case 2:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("        Nombre: " + Pachuca_Centro.getNombre());
+                                System.out.println("|--------------------------------------------------------------------------------|");
+                                System.out.println("        Direccion: " + Pachuca_Centro.getDireccion());
+                                System.out.println("        Numero De empleados: " + Pachuca_Centro.getNumeroEmpleados());
+                                System.out.println("        Cuenta: " + Pachuca_Centro.getCuenta());
+                                System.out.println("        Provedores: " + Provedor3.getRazonSocial());
+                                System.out.println("                    " + Provedor3.getDireccionFiscal());
+                                System.out.println("                    " + Provedor3.getRfc());
+                                System.out.println("                    " + Provedor3.getCadenas());
+                                System.out.println("+--------------------------------------------------------------------------------+");                                
                                 break;
                             }         
                             case 3:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("        Nombre: " + Pachuca_Sur.getNombre());
+                                System.out.println("|--------------------------------------------------------------------------------|");
+                                System.out.println("        Direccion: " + Pachuca_Sur.getDireccion());
+                                System.out.println("        Numero De empleados: " + Pachuca_Sur.getNumeroEmpleados());
+                                System.out.println("        Cuenta: " + Pachuca_Sur.getCuenta());
+                                System.out.println("        Provedores: " + Provedor2.getRazonSocial());
+                                System.out.println("                    " + Provedor2.getDireccionFiscal());
+                                System.out.println("                    " + Provedor2.getRfc());
+                                System.out.println("                    " + Provedor2.getCadenas());
+                                System.out.println("+--------------------------------------------------------------------------------+");
                                 break;
                             }         
                             case 4:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("    VOLVIENDO A ----> MENU");
+                                System.out.println("+--------------------------------------------------------------------------------+");                                
                                 break;
                             }
                             default:{
-                                
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("    OPCION INVALIDA D:");                        
+                                System.out.println("+--------------------------------------------------------------------------------+");  
                             }
                         }
                         
@@ -81,7 +119,42 @@ public class EjercicioRelaciones {
                     break;
                 }
                 case 2:{
-                    
+                    do{
+                        System.out.println("+--------------------------------------------------------------------------------+");
+                        System.out.println("    SUBMENU ----> Editar Informacion De Las Sucursales");
+                        System.out.println("|--------------------------------------------------------------------------------|");
+                        System.out.println("        (1) "+Pachuca_Norte.getNombre());
+                        System.out.println("        (2) "+Pachuca_Centro.getNombre());
+                        System.out.println("        (3) "+Pachuca_Sur.getNombre());
+                        System.out.println("        (4) SALIR");
+                        System.out.println("|--------------------------------------------------------------------------------|");
+                        System.out.println("    ELIGE UNA OPCION");
+                        System.out.println("+--------------------------------------------------------------------------------+");
+                        
+                        caso = leerInt.nextInt();
+                        
+                        switch(caso){
+                            case 1:{
+                                
+                            }
+                            case 2:{
+                                
+                            }
+                            case 3:{
+                                
+                            }
+                            case 4:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("    VOLVIENDO A ----> MENU");
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                            }
+                            default:{
+                                System.out.println("+--------------------------------------------------------------------------------+");
+                                System.out.println("    OPCION INVALIDA D:");                        
+                                System.out.println("+--------------------------------------------------------------------------------+"); 
+                            }                                
+                        }                        
+                    }while(caso != 4);                    
                     break;
                 }
                 case 3:{
